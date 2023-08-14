e786ef8c93974839d75c9c36ccd561325d4a9a85
TomaszP17 <tplucinski15@gmail.com>
2023-08-14 22:04:30 +0200

    Created FirstWindow

---
+++ summer_bootcamp_with_rafek/src/FirstWindow.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class FirstWindow {
+    private JPanel panel1;
+    private JLabel upperLabel;
+    private JPanel centerPanel;
+    private JButton logInButton;
+    private JButton SignUpButton;
+    private JButton recoverPassword;
+    private JLabel bottomLabel;
+}
