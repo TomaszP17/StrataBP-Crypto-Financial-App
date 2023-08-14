1cace929bbaadeac1e1ebcd9d2bfe8fc65bcd370
TomaszP17 <tplucinski15@gmail.com>
2023-08-14 20:57:08 +0200

    Added User class

--- summer_bootcamp_with_rafek/src/Main.java
+++ summer_bootcamp_with_rafek/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     /**
-     * Thats the first method comment to delete
+     * That's the first method comment to delete
      */
     public static void main(String[] args) {
         System.out.println("XXX");
