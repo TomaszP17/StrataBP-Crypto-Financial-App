77c0109a115bce1524c6dc1b0e7d5c83e2b20d10
TomaszP17 <tplucinski15@gmail.com>
2023-08-14 22:56:50 +0200

    Added adminPanel and new photo of admin

---
+++ summer_bootcamp_with_rafek/src/AdminPanel.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class AdminPanel extends JPanel{
+    private JPanel panel1;
+    private JLabel upperLabel;
+    private JPanel centerPanel;
+    private JButton removeButton;
+    private JButton donateButton;
+    private JButton historyButton;
+    private JButton usersButton;
+
+    public AdminPanel() {
+
+    }
+}
--- summer_bootcamp_with_rafek/src/SecondWindow.java
+++ summer_bootcamp_with_rafek/src/SecondWindow.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
+
 public class SecondWindow {
+    private JPanel panel1;
+    private JLabel upperLabel;
+    private JList centerList;
+    private JButton cou;
+    private JButton sendButton;
+    private JButton historyButton;
+    private JButton userPanelButton;
+    private JButton limitsButton;
+    private JButton donateButton;
+    private JLabel bottomLabel;
 }
