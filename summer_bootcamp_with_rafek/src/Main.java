0764688ebd76d3eb903fa8be9c04d015bad778de
Rafal2004 <rafek25521@gmail.com>
2023-08-14 21:04:23 +0200

    Added variable name into User class

--- summer_bootcamp_with_rafek/src/Main.java
+++ summer_bootcamp_with_rafek/src/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("XXX");
         //jnjinji
+        //2137
+        //33123123123
     }
 }
--- summer_bootcamp_with_rafek/src/User.java
+++ summer_bootcamp_with_rafek/src/User.java
@@ -1,3 +1,4 @@
 public class User {
     private int age;
+    private String name;
 }
